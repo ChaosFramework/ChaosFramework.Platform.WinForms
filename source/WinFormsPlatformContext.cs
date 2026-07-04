7acce47793df111b1ab0d73869d926946c8c0e7d
frameperfection <nafoizret@web.de>
2026-07-04 19:01:09 +0200

    implement PlatformContext.CreateWindow

--- source/WinFormsPlatformContext.cs
+++ source/WinFormsPlatformContext.cs
@@ -9,21 +9,23 @@ namespace ChaosFramework.Platform.WinForms
         : PlatformContext
         , GlContext
     {
+        Form MakeAGoodForm(string title)
+        {
+            Form form = new Form();
+            form.Name = title;
+            form.Show();
+            form.FormClosing += RaiseTerminate;
+            return form;
+        }
+
         Window PlatformContext.CreateWindow(string title)
-            => throw new NotImplementedException();
+            => new WinFormsWindow(MakeAGoodForm(title));
 
         Fullscreen PlatformContext.CreateFullscreen(string title, Monitor monitor)
             => CreateFullscreen(title, monitor as WinFormsMonitor ?? throw new ArgumentException($"Monitor must be a {nameof(WinFormsMonitor)}."));
 
         public WinFormsFullscreen CreateFullscreen(string title, WinFormsMonitor monitor)
-        {
-            var form = new Form();
-            form.Name = title;
-            var window = new WinFormsFullscreen(form, monitor);
-            form.Show();
-            form.FormClosing += RaiseTerminate;
-            return window;
-        }
+            => new WinFormsFullscreen(MakeAGoodForm(title), monitor);
 
         Overhead PlatformContext.messageQueue => Overhead;
 
